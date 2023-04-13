2075f336232c7110d72fc521ee9962fec5fc149b
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-13 16:31:18 +0200

    feat(esercizio)

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*console.log("ciao");*/
+
+let kmDaPercorrere = parseInt (prompt ("inserisci il numero di kilometri da percorrere"));
+
+
+let eta = parseInt (prompt("inserisci la tua età"));
+/*console.log (` l'utente dichiara l'età ${eta} `);*/
+
+let sconto20 = ( 0.21 * 0.8) ;
+let risultato20 = sconto20.toFixed(2);
+
+let sconto40 = ( 0.21 * 0.6 );
+let risultato40 = sconto40.toFixed(2);
+
+/*
+console.log (risultato20);
+console.log (risultato40);
+*/
+
+if ( eta < 18 ){
+    
+    let prezzo18 = ( kmDaPercorrere * risultato20 );
+    let risultato18 = Number (prezzo18.toFixed(2));
+    alert (`${risultato18.toFixed(2)}`);
+}
+else if ( eta > 18 && eta < 64 ) {
+    let prezzoStandard = ( kmDaPercorrere * 0.21 );
+    let risultatoStandard = Number (prezzoStandard.toFixed(2));
+    alert (`${risultatoStandard.toFixed(2)}`);
+}
+else ( eta > 65 ); {
+    let prezzo65 = ( kmDaPercorrere * risultato40 );
+    let risultato65 = Number (prezzo65.toFixed(2));
+    alert (`${risultato65.toFixed(2)}`);
+}
+
